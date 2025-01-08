1e8518e5ebdd371f45ec69c72672a3a60536929e
Ellen <ellenrichterjonsson@gmail.com>
2025-01-08 10:30:56 +0100

    Funktion createCityBoxes som skapar rutorna för städerna

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Recommended: All functions declared here
+function createCityBoxes(cityName) {
+    const cityBoxDiv = document.createElement("div");
+    cityBoxDiv.classList.add("cityBox");
+    cityBoxDiv.textContent = cityName;
+    document.getElementById("cities").append(cityBoxDiv);
+}
 
 // Recommended: constants with references to existing HTML-elements
 
