3d763f2fd5da7c2f5f13bd729f92f55237635531
Ellen <ellenrichterjonsson@gmail.com>
2025-01-08 10:37:16 +0100

    Funktion displayAllCities för att iterera över cities och skapa rutor

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function createCityBoxes(cityName) {
     document.getElementById("cities").append(cityBoxDiv);
 }
 
+function displayAllCities() {
+    const citiesContainer = document.getElementById("cities");
+
+    for (let i = 0; i < cities.length; i++) {
+        createCityBoxes(cities[i].name);
+    }
+}
+displayAllCities();
 // Recommended: constants with references to existing HTML-elements
 
 // Recommended: Ask for the city name and then the rest of the code
